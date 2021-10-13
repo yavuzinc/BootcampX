79614a0c2f8bc378d35af10b33b3edb1368fb1ff
Yavuz Incedal <yavuzincedalca@gmail.com>
2021-10-13 17:24:07 -0400

    refactoring code

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,18 +1,22 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'yavuzincedal',
+  user: 'vagrant',
   password: '123',
   host: 'localhost',
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+WHERE cohorts.name LIKE $1
+LIMIT $2;
 `)
 .then(res => {
   res.rows.forEach(user => {
